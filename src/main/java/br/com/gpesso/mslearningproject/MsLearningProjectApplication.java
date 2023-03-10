e651dffd0664a3e5b8cf2b66ebdd73ea13432bbe
Gabriel Loiola <>
2023-03-10 19:52:37 -0300

    Adiciona projeto inicial

---
+++ src/main/java/br/com/gpesso/mslearningproject/MsLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.gpesso.mslearningproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLearningProjectApplication.class, args);
+	}
+
+}
